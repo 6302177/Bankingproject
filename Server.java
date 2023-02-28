d1bfea6ba839e38a4aa1b3c64cbc9db8b0b302ff
6302177 <110083829+6302177@users.noreply.github.com>
2023-02-28 13:26:02 +0530

    Add files via upload

---
+++ Server.java
@@ -0,0 +1,23 @@
+package src.atmappmain;
+
+import java.util.Scanner;
+
+public abstract class Server
+{
+	public int uid=12345;
+	public int upw=123;
+	public int id;
+	public int pw;
+	public double amount;
+	static double bankBalance=1000000;
+	Scanner s=new Scanner(System.in);
+	void input()
+	{
+		System.out.println("Enter the UserID");
+		id=s.nextInt();
+		System.out.println("Enter the PassWord");
+		pw=s.nextInt();
+	}
+	abstract void verify() throws Exception;
+	
+}
